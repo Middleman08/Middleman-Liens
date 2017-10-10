7e0233c9e9675a47c1b0e568355272bec89f0984
Middleman08 <middleman08@yahoo.com>
2017-10-09 18:03:04 -0700

    Add files via upload
    final version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // Make it rain!
 
+// hamburger and side bar code
 $('.hamburger').on('click', function(){
 	console.log("clicked");
 
@@ -13,11 +14,43 @@ $('.close').on('click', function(){
 	
 });
 
+
+//Google map code
 function myMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(37.5833637,-122.4758374),
-        zoom: 10,
+        center: new google.maps.LatLng(37.583312,-122.475328),
+        zoom: 80,
         mapTypeId: google.maps.MapTypeId.HYBRID
     }
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }
+
+
+// this is an offset for the fixed navigation
+$('a[href*="#"]:not([href="#"])').click(function() {
+
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+       var target = $(this.hash);
+
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+       if (target.length) {
+           $('html, body').animate({
+               scrollTop: target.offset().top-130 //this is the offset to change
+           }, 1000);
+           return false;
+       }
+   }
+});
+
+
+//this is the code for my modal order form
+$('#showModal').on('click', function () {
+  $('.modal').css("display", "flex").hide().fadeIn();
+  console.log('click');
+});
+
+$('#close').on('click', function () {
+  $('.modal').fadeOut();
+  console.log('click');
+  
+});
